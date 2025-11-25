e73aab288e7ba1237ef4ec62e784a2d536c0f28c
Jonathan Dickert <jonathan.dickert@protonmail.com>
2025-11-24 22:30:24 -0500

    change sqlite db name again

--- crates/beditor-lib/src/util/storage.rs
+++ crates/beditor-lib/src/util/storage.rs
@@ -34,7 +34,7 @@ impl Storage {
       .file_stem()
       .expect("failed to get current executable file stem");
 
-    let filename = format!("{}-beditor-settings.sqlite", stem.display());
+    let filename = format!("{}_beditor_settings.sqlite", stem.display());
     current_exe.parent().unwrap().to_path_buf().join(filename)
   }
 }

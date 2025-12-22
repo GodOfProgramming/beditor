05c6840b07b263cfea34834ef8b00c6f545959ac
Jonathan Dickert <jonathan.dickert@protonmail.com>
2025-12-21 21:50:07 -0500

    improved identifiable macro

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,7 +1,8 @@
 use proc_macro::TokenStream;
-use proc_macro2::Literal;
+use proc_macro_crate::{FoundCrate, crate_name};
+use proc_macro2::{Literal, Span};
 use quote::quote;
-use syn::{DeriveInput, Lit, parse_macro_input};
+use syn::{DeriveInput, Ident, Lit, parse_macro_input};
 
 #[proc_macro_derive(Identifiable, attributes(id))]
 pub fn identifiable(input: TokenStream) -> TokenStream {
@@ -24,9 +25,18 @@ pub fn identifiable(input: TokenStream) -> TokenStream {
 		panic!("Missing valid #[id(\"...\")] attribute");
 	};
 
+	let crate_name =
+		crate_name("beditor").expect("beditor should be present for this macro to be used");
+
+	let span = Span::call_site();
+	let beditor = match crate_name {
+		FoundCrate::Itself => Ident::new("crate", span),
+		FoundCrate::Name(name) => Ident::new(&name, span),
+	};
+
 	let expanded = quote! {
-		impl beditor::Identifiable for #name {
-			const ID: beditor::uuid::Uuid = beditor::uuid::uuid!(#id_attr);
+		impl #beditor::Identifiable for #name {
+			const ID: #beditor::uuid::Uuid = #beditor::uuid::uuid!(#id_attr);
 			const TYPE_NAME: &'static str = #name_lit;
 		}
 	};
